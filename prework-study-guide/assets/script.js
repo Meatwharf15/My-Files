d7afba5d41339eb879125cba13e1f5386c905a4f
Zach Gilmore <zachgilmore@Zachs-MBP-2.lan>
2024-09-30 00:58:06 -0500

    learning basic Javascript

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,21 @@
+const topics = ["HTML", "CSS", "Git", "Javascript"];
+
+for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+if (topics === "HTML") {
+    console.log("Let's study HTML!")
+}
+
+else if (topics === "CSS") {
+    console.log("Let's study CSS!")
+}
+
+else if (topics === "Git") {
+    console.log("Let's study Git!")
+}
+
+else if (topics === "Javascript") {
+    console.log("Let's study Javascript!")
+}
